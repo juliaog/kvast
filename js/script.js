e4fce0b9d5ca9fa6cea44669e671c9716960bea6
juliaog <juliaog93@gmail.com>
2019-06-26 15:38:02 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var mass = [
+    'Крафтовый квас <br> всему голова',
+    'Крафтовый квас <br> всему голова,а некоторым и тело!',
+    'Крафтовый квас <br> всему голова, а хлеб тоже'
+];
+
+var texts = document.querySelectorAll('.slide-icon');
+var oneText = document.querySelector('.change');
+
+console.log(this);
+
+function onSlideIconClick() {
+
+}
+
+for (var i = 0; i < texts.length; i++) {
+
+}
+
+
